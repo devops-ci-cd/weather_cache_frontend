c190de17809e9bfa3b6841a4ed8c61fa9b210327
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-25 10:54:58 +1200

    the data is ordered, the form works

--- django_project_frontend/api_query/forms.py
+++ django_project_frontend/api_query/forms.py
@@ -9,6 +9,6 @@ class main_form(forms.Form):
         'widget': BootstrapDateTimePickerInput(),
         'initial': datetime.today().strftime('%d.%m.%Y'),
     }
-    date_from = forms.DateField(**options)
+    date = forms.DateField(**options)
     # date_to = forms.DateField(**options)
-    city = forms.CharField(initial='St. Petersburg')
+    city = forms.CharField(initial='St. Petersburg', disabled=True)
--- django_project_frontend/api_query/views.py
+++ django_project_frontend/api_query/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .forms import main_form
 from .models import Weathercache
 
+from azure.servicebus import ServiceBusClient, ServiceBusMessage
+import os
+
+connstr = os.environ['SERVICE_BUS_CONNECTION_STR']
+queue_name = os.environ['SERVICE_BUS_QUEUE_NAME']
+
 def main_view(request):
+    if request.method == 'POST':
+        if (len(request.POST['date']) <= 10):
+            with ServiceBusClient.from_connection_string(connstr) as client:
+                with client.get_queue_sender(queue_name) as sender:
+                    # Sending a single message
+                    single_message = ServiceBusMessage(''.join(filter(str.isdigit, request.POST['date'])))
+                    sender.send_messages(single_message)
+                    return HttpResponseRedirect('/')
+        else: pass
     context = {}
-    data = Weathercache.objects.using('data').all()
+    data = Weathercache.objects.using('data').order_by('date__month', 'date__day', 'date__year')
     context = {'data': data,
         'form': main_form()
     }
