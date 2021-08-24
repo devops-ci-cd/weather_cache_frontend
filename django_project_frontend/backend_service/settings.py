ca2d761258d8fdb05dd70acc2baa5678215c6863
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-25 11:22:34 +1200

    debug -> false

--- django_project_frontend/backend_service/settings.py
+++ django_project_frontend/backend_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '9(!forskpj_81&g0f9%4yepmtxc7fc6b-zud*3!i4%le58ucnf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
