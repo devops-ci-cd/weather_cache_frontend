3e1a5acba7c7168cfebde8cde7f2c694d6383425
Evgeniy Polyarush <bratikha@gmail.com>
2021-08-26 15:42:00 +1200

    Hide the secret

--- django_project_frontend/backend_service/settings.py
+++ django_project_frontend/backend_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9(!forskpj_81&g0f9%4yepmtxc7fc6b-zud*3!i4%le58ucnf'
+SECRET_KEY = getenv('django_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
